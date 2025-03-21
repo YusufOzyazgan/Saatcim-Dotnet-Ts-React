197d1cb64b79ebe147ffbff5de0dac737507698d
Yusuf Özyazgan <yusufozyazgan@outlook.com>
2025-03-21 11:44:36 +0300

    Kullanici-islemleri-sonu

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ef4c8400ac0a7688aab696929cd1df2e86d2bc4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a210649fb88cb1abc9e51bb071f7c5adb2f726d")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
